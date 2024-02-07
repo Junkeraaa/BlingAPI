209dd257304c394efa0ece04c90fa78f9f9d2ca8
Lucas Junker Narais <lucasnarvais@gmail.com>
2024-02-06 23:58:23 -0300

    testes

--- src/controller/auth/authorizationController.js
+++ src/controller/auth/authorizationController.js
@@ -18,7 +18,7 @@ server.get("/authorization", async (req, res) => {
             
             const authorizationCode = await getAuthorizationCode(code, system);
             res.writeHead(301, {
-                Location: `https://65c1a985d33d0f25612fdadd--clever-queijadas-65043d.netlify.app/q?system=${system}&token=${authorizationCode}`
+                Location: `https://65c2f17910bee7246345ebcd--stirring-kleicha-50756e.netlify.app/q?system=${system}&token=${authorizationCode}`
               }).end();
         }
         res.send();
