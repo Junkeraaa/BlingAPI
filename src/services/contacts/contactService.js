285c8a46ccf373fa6163296200d2696980db2684
lupinha1 <lupinha2014@gmail.com>
2024-02-07 01:17:58 -0300

    BranchLuis

--- src/services/contacts/contactService.js
+++ src/services/contacts/contactService.js
@@ -1,60 +1,20 @@
 import axiosInstanceCode from '../../connection/axios.js';
 
-export async function getAllContacts(authorizationCode) {
-    let allContacts = [];
-    let pageNumber = 1;
-
-    try {
-        while (true) {
-            const response = await axiosInstanceCode.get('contatos', {
-                params: {
-                    pagina: pageNumber,
-                    limite: 100 
-                },
-                headers: {
-                    'Authorization': `Bearer ${authorizationCode}`
-                }
-            });
-
-            if (response.data.data.length === 0) {
-                
-                break;
-            }
-
-            allContacts = allContacts.concat(response.data.data);
-            pageNumber++;
-        }
-
-        return allContacts;
-    } catch (error) {
-        console.error("Erro na solicitação:", error.response.data.error);
-        return [];
-    }
-}
-
-
-
-
-export async function getContactId(clientName, allContacts) {
+export async function getContactId(clientName, authorizationCode) {
     // console.log("Acessando id do cliente!");
 
     try {
         // const allContacts = await getAllContacts(authorizationCode);
-        let contactId = 0;
-
-        for (let i = 0; i < allContacts.length; i++) {
-            const contact = allContacts[i];
-            if (contact.nome.toLowerCase() === clientName.toLowerCase()) {
-                contactId = contact.id;
-                break;
+        let contactId;
+        const response = await axiosInstanceCode.get(`contatos?pesquisa=${clientName}`, {
+            headers: {
+                'Authorization': `Bearer ${authorizationCode}`
             }
-        }
-
-       // console.log(`Total de contatos: ${allContacts.length}`);
-       // console.log(`ID do contato: ${contactId}`);
+        });
+        contactId = response.data.id
         return contactId;
     } catch (error) {
-        console.error("Erro na solicitação:", error.response.data.error.fields[0]);
+        console.error("Erro na solicitação:", error.response.data.error);
         return null;
     }
 }
--- src/services/product/productService.js
+++ src/services/product/productService.js
@@ -1,58 +1,16 @@
 import axiosInstanceCode from '../../connection/axios.js';
 import removerAcentos from '../common/functions.js';
 
-export async function getAllProducts(authorizationCode) {
-    let allProducts = [];
-    let pageNumber = 1;
-
-    try {
-        while (true) {
-            const response = await axiosInstanceCode.get('produtos', {
-                params: {
-                    pagina: pageNumber,
-                    limite: 100
-                },
-                headers: {
-                    'Authorization': `Bearer ${authorizationCode}`
-                }
-            });
-
-            if (response.data.data.length === 0) {
-                
-                break;
-            }
-
-            allProducts = allProducts.concat(response.data.data);
-            pageNumber++;
-        }
-
-        return allProducts;
-
-    } catch (error) {
-        console.error("Erro na solicitação:", error.response.data.error.fields[0]);
-        return [];
-    }
-}
-
-
-
-
-export async function getProductId(allProducts, productName) {
+export async function getProductId(productName) {
     // console.log("Acessando id do produto!");
 
     try {
-        // const allProducts = await getAllProducts(authorizationCode);
-        let productId = 0;
-
-        for (let i = 0; i < allProducts.length; i++) {
-            const product = allProducts[i];
-            if (removerAcentos(product.nome.toLowerCase()) === removerAcentos(productName.toLowerCase())) {
-                productId = product.id;
-                break;
+        let productId;
+        const response = await axiosInstanceCode.get(`produtos?nome=${productName}`, {
+            headers: {
+                'Authorization': `Bearer ${authorizationCode}`
             }
-        }
-
-        //console.log(`Total de produtos: ${allProducts.length}`);
+        });
         return productId;
     } catch (error) {
         console.error("Erro na solicitação:", error.response.data.error.fields[0]);
